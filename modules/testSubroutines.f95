3b477ff517b65b2bc858487096ec110a8822e05a
nbcallah <nbcallah@indiana.edu>
2017-12-19 13:39:36 -0500

    created dip timing arrays before checking against them (this usually helps)

--- modules/testSubroutines.f95
+++ modules/testSubroutines.f95
@@ -23,22 +23,22 @@ SUBROUTINE zOffDipCalc(t, z)
 	real(kind=PREC), intent(in) :: t
 	real(kind=PREC), intent(out) :: z
 	
-	integer :: nDips = 10
+	integer :: nDips = 5
 	real(kind=PREC) :: speed
-	real(kind=PREC), dimension(10) :: dipHeights
-	real(kind=PREC), dimension(10) :: dipEnds
+	real(kind=PREC), dimension(5) :: dipHeights
+	real(kind=PREC), dimension(5) :: dipEnds
 	
 	integer :: i
+	
+!	dipHeights = (/0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010/)
+	dipHeights = (/0.49_8, 0.380_8, 0.250_8, 0.180_8, 0.01_8/)
+!	dipEnds =     (/0.0,  40.0,  80.0,  100.0, 120.0, 140.0, 160.0, 180.0, 200.0, 300.0/)
+	dipEnds =     (/0.0_8,  40.0_8, 80.0_8,  400.0_8, 500.0_8/)
 	
 	IF (t > dipEnds(nDips)) THEN
 		z = 0.01
 		RETURN
 	END IF
-	
-!	dipHeights = (/0.49, 0.380, 0.250, 0.180, 0.140, 0.110, 0.080, 0.060, 0.040, 0.010/)
-	dipHeights = (/0.49, 0.380, 0.250, 0.180, 0.01/)
-!	dipEnds =     (/0.0,  40.0,  80.0,  100.0, 120.0, 140.0, 160.0, 180.0, 200.0, 300.0/)
-	dipEnds =     (/0.0,  40.0, 80.0,  400.0, 500.0/)
 	
 	speed = 0.49_8/13.0_8
 	
